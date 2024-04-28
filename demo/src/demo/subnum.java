f836b679d97f7ca894e262b4ea5f1d996581898b
SAENRO GONSALVES <SAENRO GONSALVES@LAPTOP-SAEN>
2024-04-28 12:27:37 +0530

    firts cvommit

---
+++ demo/src/demo/subnum.java
@@ -0,0 +1,41 @@
+package demo;
+
+import java.util.Scanner;
+
+public class subnum {
+	
+	int sum1;
+	float orange_juice,Godrej;
+	
+	subnum(int sum1)
+	{
+		this.sum1=sum1;
+	}
+	void add_water() {
+		
+	}
+	void display() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x,y,sum;
+		
+		System.out.println( "enter the numbers");
+		Scanner sc = new Scanner(System.in);
+		x=sc.nextInt();
+		y=sc.nextInt();
+		sum=x+y;
+		System.out.println("the sum is "+sum);
+		
+	
+			
+			
+		}
+		
+	
+
+	
+
+}
